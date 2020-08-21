0e64b293c8b040df6ef3e6a50f214cffdbd8099b
JaWa3107 <70014344+JaWa3107@users.noreply.github.com>
2020-08-21 17:13:08 +0200

    Add FXML Design

---
+++ QuiziApp/src/main/java/com/QuiziApp/ControllerFXML.java
@@ -0,0 +1,81 @@
+package com.QuiziApp;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.QuiziApp.HelperClasses.ConvertWordDocToText;
+import com.QuiziApp.HelperClasses.WordsearchAlgorithm;
+import com.QuiziApp.Models.QAModel;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+public class ControllerFXML {
+
+	
+	// Properties
+	Label showWordText = new Label();
+	Stage primaryStage = new Stage();
+	
+
+	// Methods
+	
+    @FXML
+    private Button createButton;
+    
+    @FXML
+    void createButtonTapped(ActionEvent event) {
+
+    	System.out.println("test");
+    	
+    	// FileChooser wird erstellt, damit wir ein Word file aussuchen k√∂nnen
+		FileChooser fileChooser = new FileChooser();
+		setupFilter(fileChooser);
+		File wordFile= fileChooser.showOpenDialog(primaryStage);
+		System.out.print(wordFile.getAbsolutePath());
+		ConvertWordDocToText text = new ConvertWordDocToText();
+		WordsearchAlgorithm search = new WordsearchAlgorithm();
+
+		
+		try {
+			showWordText.setText(text.getText(wordFile));
+			//search.filter(text.getText(wordFile));
+			
+			ArrayList<QAModel>  questions = search.filterQuestionsFromPackage(text.getText(wordFile));
+			
+			saveQuestion(questions);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+    
+    
+    private void setupFilter(FileChooser fileChooser) {
+		fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Word document", "*.docx"));
+	}
+	
+	public void saveQuestion(ArrayList<QAModel> pack) throws IOException {
+		File file = new File("DerPenisIst5cmGroﬂ.json");
+		
+		// Neuen Ordner erstellen
+//		File dir = new File("Folder");
+//		dir.mkdir();
+		
+		var writer = new ObjectMapper();
+		writer.enable(SerializationFeature.INDENT_OUTPUT);
+		writer.writeValue(file, pack);
+		System.out.print("Success");
+	}
+	
+	}
+
--- QuiziApp/src/main/java/com/QuiziApp/QuiziMain.java
+++ QuiziApp/src/main/java/com/QuiziApp/QuiziMain.java
@@ -13,6 +13,8 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -23,73 +25,23 @@ import javafx.stage.Stage;
 
 public class QuiziMain extends Application { 
 
-	Button wordFileButton = new Button("Choose Word File");
-	Label showWordText = new Label();
+	// Instanz der Klasse ControllerFXML
+	ControllerFXML controller = new ControllerFXML();
 
 	@Override
 	public void start(final Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
 		
-		//BorderPane root  = new BorderPane();
-		VBox root = new VBox();
+		Parent root = FXMLLoader.load(getClass().getResource("homeFXML.fxml"));
 		
+		// hier übergeben wir die primaryStage an der Variable primaryStage aus der Instanz controller
+		controller.primaryStage = primaryStage;
 		
-		wordFileButton.setOnAction(new EventHandler<ActionEvent>() {
-			
-			public void handle(ActionEvent arg0) {
-				// TODO Auto-generated method stub
-				
-				// FileChooser wird erstellt, damit wir ein Word file aussuchen kÃ¶nnen
-				FileChooser fileChooser = new FileChooser();
-				setupFilter(fileChooser);
-				File wordFile= fileChooser.showOpenDialog(primaryStage);
-				System.out.print(wordFile.getAbsolutePath());
-				ConvertWordDocToText text = new ConvertWordDocToText();
-				WordsearchAlgorithm search = new WordsearchAlgorithm();
-	
-				
-				try {
-					showWordText.setText(text.getText(wordFile));
-					//search.filter(text.getText(wordFile));
-					
-					ArrayList<QAModel>  questions = search.filterQuestionsFromPackage(text.getText(wordFile));
-					
-					saveQuestion(questions);
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-		 
-				
-				
-			}
-		});
-		
-		root.getChildren().addAll(wordFileButton, showWordText);
-		
-		Scene scene = new Scene(root, 400, 400);
+		Scene scene = new Scene(root, 800, 500);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
-	
-	private void setupFilter(FileChooser fileChooser) {
-		fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Word document", "*.docx"));
-	}
-	
-	public void saveQuestion(ArrayList<QAModel> pack) throws IOException {
-		File file = new File("test2.json");
-		
-		// Neuen Ordner erstellen
-//		File dir = new File("Folder");
-//		dir.mkdir();
-		
-		var writer = new ObjectMapper();
-		writer.enable(SerializationFeature.INDENT_OUTPUT);
-		writer.writeValue(file, pack);
-		
-			
-		
-	}
+
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
