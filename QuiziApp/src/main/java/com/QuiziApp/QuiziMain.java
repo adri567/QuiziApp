44c377faa3073f41fa20b2e06ecf275d9cf3c580
JaWa3107 <70014344+JaWa3107@users.noreply.github.com>
2020-08-21 13:04:59 +0200

    hallo

---
+++ QuiziApp/src/main/java/com/QuiziApp/QuiziMain.java
@@ -0,0 +1,27 @@
+package com.QuiziApp;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class QuiziMain extends Application { 
+
+	
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+		BorderPane root  = new BorderPane();
+		
+		Scene scene = new Scene(root, 800, 800);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+}
