8d227d438bffa3f8ac76a7263b6e6b42886c158d
JaWa3107 <70014344+JaWa3107@users.noreply.github.com>
2020-08-24 18:40:43 +0200

    Custom Cell ListView issue fixed
    Neuer issue gefunden

--- QuiziApp/src/main/java/com/QuiziApp/SectionListViewCellController.java
+++ QuiziApp/src/main/java/com/QuiziApp/SectionListViewCellController.java
@@ -6,10 +6,14 @@ import com.QuiziApp.Models.SectionModel;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
+import javafx.scene.layout.HBox;
 
 public class SectionListViewCellController extends ListCell<SectionModel> {
+
+	
 
 	// Properties
 	
@@ -17,31 +21,45 @@ public class SectionListViewCellController extends ListCell<SectionModel> {
 		System.out.print("Test");
 	}
 	
-	 public SectionListViewCellController() {
-	        loadFXML();
-	    }
-
-	    private void loadFXML() {
-	        try {
-	            FXMLLoader loader = new FXMLLoader(getClass().getResource("sectionFXML.fxml"));
-	            loader.setController(this);
-	            loader.setRoot(this);
-	            loader.load();
-	        }
-	        catch (IOException e) {
-	            throw new RuntimeException(e);
-	        }
-	    }
+	@FXML
+	private HBox Hbox;
 	
+	@FXML
+	private FXMLLoader mLLoader;
+		
+	// Diese Methode Läd die "sectionFXML" datei und gibt die Custom Cell in der listView auf der Startseite aus.
 	    @Override
 	    protected void updateItem(SectionModel section, boolean empty) {
 	        super.updateItem(section, empty);
 
-	   
-	            sectionName.setText(section.getSectionName());
-	        
-	    }
-	
+	   if(empty || section == null) {
+		   
+		   setText(null);
+		   
+	   }else {
+		   
+		   if(mLLoader == null) {
+			   
+			   mLLoader = new FXMLLoader(getClass().getResource("sectionFXML.fxml"));
+			   mLLoader.setController(this);
+			   
+			   try {
+				   mLLoader.load();
+			   }catch(IOException e) {
+				   e.printStackTrace();
+			   }
+			 }
+			
+		   sectionName.setText(section.getSectionName());
+		   setText(null);
+		   // Setzt die Hbox in den Vordergrund
+		   setGraphic(Hbox);
+		   
+		   }
+		   
+	   }
+		   
+
 	
     @FXML
     private Label sectionName;
